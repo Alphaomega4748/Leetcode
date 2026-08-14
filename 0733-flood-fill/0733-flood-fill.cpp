2e4ab5c1f24c6f68c3150c45f509366356ffac2e
Akash Kumar Das <akashkumar942759@gmail.com>
2026-08-14 14:39:04 +0530

    Time: 0 ms (100%), Space: 18.2 MB (43.35%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,49 +1,41 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image,
-                                   int sr, int sc, int color) {
-
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        
         int n = image.size();
         int m = image[0].size();
+        int old_color = image[sr][sc];
 
-        int oldColor = image[sr][sc];
-
-        if (oldColor == color) {
+        if(old_color == color){
             return image;
         }
 
-        queue<pair<int, int>> q;
+        queue<pair<int,int>> q;
+        q.push({sr,sc});
 
-        q.push({sr, sc});
         image[sr][sc] = color;
 
-        int dx[] = {-1, 0, 1, 0};
-        int dy[] = {0, 1, 0, -1};
+        int dx[] = {-1,0,1,0};
+        int dy[] = {0,1,0,-1};
 
-        while (!q.empty()) {
-
+        while(!q.empty()){
             int x = q.front().first;
             int y = q.front().second;
             q.pop();
+            
+            for(int i =0; i<4; i++){
+                int nRow = x + dx[i];
+                int nCol = y + dy[i];
 
-            for (int i = 0; i < 4; i++) {
-
-                int newX = x + dx[i];
-                int newY = y + dy[i];
-
-                if (newX >= 0 && newX < n &&
-                    newY >= 0 && newY < m) {
-
-                    if (image[newX][newY] == oldColor) {
-
-                        image[newX][newY] = color;
-
-                        q.push({newX, newY});
-                    }
+                if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && image[nRow][nCol] == old_color){
+                    image[nRow][nCol] = color;
+                    q.push({nRow,nCol});
                 }
             }
+
         }
 
         return image;
+
     }
 };
